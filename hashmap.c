0f7ee05cad508a4a7728f894e824297497cb505f
Replit user <>
2023-09-08 19:42:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -90,37 +90,38 @@ HashMap * createMap(long capacity) {
 }
 
 void eraseMap(HashMap * map,  char * key) {    
-   if (map == NULL || key == NULL) {
-        return;  // Verificar entradas inválidas
+ if (map == NULL || key == NULL) {
+      return;  // Verificar entradas inválidas
+  }
+
+  long index = hash(key, map->capacity);
+
+  if (map->buckets[index] != NULL) {
+    // Si el bucket no está vacío, verifica si la clave coincide
+    if (is_equal(map->buckets[index]->key, key)) {
+      // La clave coincide, libera la memoria del par clave-valor y marca el bucket como vacío
+      free(map->buckets[index]->key);
+      free(map->buckets[index]);
+      map->buckets[index] = NULL;
+      map->size--;
+      return;
+  } else {
+    // La clave no coincide, busca en los siguientes buckets (si hay colisiones)
+    long nextIndex = (index + 1) % map->capacity;
+    while (nextIndex != index) {
+      if (map->buckets[nextIndex] != NULL && is_equal(map->buckets[nextIndex]->key, key)) {
+        // Se encontró la clave en un bucket posterior, libera la memoria del par clave-valor y marca el bucket como vacío
+        free(map->buckets[nextIndex]->key);
+        free(map->buckets[nextIndex]);
+        map->buckets[nextIndex] = NULL;
+        map->size--;
+        return;
+      }
+      // Intenta el siguiente bucket en el ciclo
+      nextIndex = (nextIndex + 1) % map->capacity;
+      }
     }
-
-    long index = hash(key, map->capacity);
-
-    if (map->buckets[index] != NULL) {
-        // Si el bucket no está vacío, verifica si la clave coincide
-        if (is_equal(map->buckets[index]->key, key)) {
-            // La clave coincide, libera la memoria y marca el bucket como vacío
-            free(map->buckets[index]);
-            map->buckets[index] = NULL;
-            map->size--;
-            return;
-        } else {
-            // La clave no coincide, busca en los siguientes buckets (si hay colisiones)
-            long nextIndex = (index + 1) % map->capacity;
-            while (nextIndex != index) {
-                if (map->buckets[nextIndex] != NULL && is_equal(map->buckets[nextIndex]->key, key)) {
-                    // Se encontró la clave en un bucket posterior, libera la memoria y marca el bucket como vacío
-                    free(map->buckets[nextIndex]);
-                    map->buckets[nextIndex] = NULL;
-                    map->size--;
-                    return;
-                }
-                // Intenta el siguiente bucket en el ciclo
-                nextIndex = (nextIndex + 1) % map->capacity;
-            }
-        }
-    }
-
+  }
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
